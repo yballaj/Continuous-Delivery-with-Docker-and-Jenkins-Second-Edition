bed1de44c55b4c2e93a72b49f48baba0621346f3
Yassine Ballaj <ballajyassine@gmail.com>
2024-02-19 23:14:47 -0500

    Update Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -6,6 +6,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
+
+	 /**
+            * Sums two integers.
+            * @param a the first integer
+            * @param b the second integer
+            * @return the sum of a and b
+        */
 	@Cacheable("sum")
 	public int sum(int a, int b) {
 		return a + b;
