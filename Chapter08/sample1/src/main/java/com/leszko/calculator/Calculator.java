b49432d75f38c367403f53ec01fab9908b42988b
Yassine Ballaj <ballajyassine@gmail.com>
2024-02-26 21:29:31 -0500

    Update Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -20,7 +20,11 @@ public class Calculator {
           */
 	@Cacheable("sum")
 	public int sum(int a, int b) {
-		//Print value of a
+		/**
+                 * This method prints the value of variable 'a' to the console.
+                 * It constructs a string that includes the variable's name and its value, 
+                 * then prints this information to standard output, allowing the user to see the current value of 'a'.
+                */
 		System.out.println("Value of a: " + a);
                
 		return a + b;
