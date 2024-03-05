123604a760e8f82906ef5b053a2302561dea84de
Yassine Ballaj <ballajyassine@gmail.com>
2024-03-04 22:14:46 -0500

    Update Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,9 +3,6 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-/**
- * Calculator service that provides basic arithmetic operations.
- */
 
 @Service
 public class Calculator {
