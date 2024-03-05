489776132f9877d17048d44fa0a8df36ce5e00b6
Yassine Ballaj <ballajyassine@gmail.com>
2024-03-04 21:14:21 -0500

    Update Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Calculator {
-        final static int UMLNUM1 = 3; // Renamed constant
+        final static int UMLNUM1 = 3; 
 
 
 	@Cacheable("sum")
